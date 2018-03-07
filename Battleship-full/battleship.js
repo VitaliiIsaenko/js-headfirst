61f475b46a9ef87b1c4e40705df3cab92078a40a
Vitalii Isaenko <lookyisaenko@gmail.com>
2018-03-07 10:50:25 +0100

    Implement guess parsing

--- Battleship-full/battleship.js
+++ Battleship-full/battleship.js
@@ -1,42 +1,36 @@
 var view = {
-    displayMessage: function(msg) {
+    displayMessage: function (msg) {
         var messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
     },
 
-    displayHit: function(location) {
+    displayHit: function (location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "hit");
     },
 
-    
-    displayMiss: function(location) {
+
+    displayMiss: function (location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "miss");
     }
 }
 
-view.displayMiss("00");
-view.displayMiss("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMessage("Tap tap, is this thing on?");
-
 var model = {
-    boardSize:7,
-    numShips:3,
+    boardSize: 7,
+    numShips: 3,
     shipLength: 3,
     shipsSunk: 0,
 
-    ships:[{locations: ["06", "16", "26"], hits: ["", "", ""]},
-    {locations: ["24", "34", "44"], hits: ["", "", ""]},
-    {locations: ["10", "11", "12"], hits: ["", "", ""]}],
+    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] }],
 
-    fire: function(guess) {
-        for(var i = 0; i < this.numShips; i++) {
+    fire: function (guess) {
+        for (var i = 0; i < this.numShips; i++) {
             var index = this.ships[i].locations.indexOf(guess);
-            if(index >= 0) {
-                ship.hits[index] = "hit";
+            if (index >= 0) {
+                this.ships[i].hits[index] = "hit";
                 view.displayHit(guess);
                 view.displayMessage("Hit!");
                 if (this.isSunk(ship)) {
@@ -47,16 +41,47 @@ var model = {
             }
         }
         view.displayMiss(guess);
-        view.displayMessage("You missed");
+        view.displayMessag e("You missed");
         return false;
     },
 
-    isSunk: function(ship) {
-        for(var i = 0; i < ship.locations; i++) {
+    isSunk: function (ship) {
+        for (var i = 0; i < ship.locations; i++) {
             if (ship.locations[i] !== "hit") {
                 return false;
             }
         }
         return true;
     }
+}
+
+var controller = {
+    guesses: 0,
+
+    processGuess: function (guess) {
+
+
+    }
+
+
+}
+
+function parseGuess(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    if (guess === null || guess.length !== 2) {
+        alert("oops! Please, enter a letter and a number on the board");
+    } else {
+        firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+
+        if (isNan(row) || isNan(column)) {
+            alert("Oops, that isn't on the board");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert("Ooops, that's off the board!");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
 }
