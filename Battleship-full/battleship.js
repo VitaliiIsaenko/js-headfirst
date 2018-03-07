010cd3dada4984ff88284faee2007e8991b8fe3d
Vitalii Isaenko <lookyisaenko@gmail.com>
2018-03-07 12:14:15 +0100

    Implement collistion, fix bug in ship generation

--- Battleship-full/battleship.js
+++ Battleship-full/battleship.js
@@ -5,6 +5,8 @@ function init() {
     fireButton.onclick = handleFireButton;
     var guessInput = document.getElementById("guessInput");
     guessInput.onkeypress = handleKeyPress;
+
+    model.generateShipLocations();
 }
 
 var view = {
@@ -31,9 +33,9 @@ var model = {
     shipLength: 3,
     shipsSunk: 0,
 
-    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-    { locations: ["24", "34", "44"], hits: ["", "", ""] },
-    { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    ships: [{ locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] }],
 
     fire: function (guess) {
         for (var i = 0; i < this.numShips; i++) {
@@ -82,23 +84,32 @@ var model = {
         if (direction === 1){
             row = Math.floor(Math.random() * this.boardSize);
             col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
-        } else () {
-
+        } else {
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+            col = Math.floor(Math.random() * this.boardSize);
         }
 
         var newShipLocations = [];
         for (var i = 0; i < this.shipLength; i++) {
             if (direction === 1) {
-                newShipLocations.push(row + " " + (col + i));
+                newShipLocations.push(row + "" + (col + i));
             } else {
-                newShipLocations.push((row + i) + " " + col);
+                newShipLocations.push((row + i) + "" + col);
             }
         }
         return newShipLocations;
     },
 
-    collision() {
-
+    collision: function(locations) {
+        for (var i = 0; i < this.numShips; i++){
+            var ship = model.ships[i];
+            for (var j = 0; j < locations.length; j++){
+                if (ship.locations.indexOf(locations[j]) >= 0){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
